326c434db6826f9f1fa84182ba5b9f311896d944
Nonato Falcão <raimundo23070070@aluno.cesupa.br>
2025-10-01 00:21:46 -0300

    Correção

--- app.py
+++ app.py
@@ -72,14 +72,11 @@ genres_list = list(charts_json.keys())
 # Slider com emojis de seta para indicar seleção
 index = st.slider("Escolha o gráfico pelo índice ⬅️➡️", 0, len(genres_list)-1, 0)
 
-if genres_list:  # só entra se a lista não estiver vazia
-    index = st.slider("Escolha o gráfico pelo índice ⬅️➡️", 0, len(genres_list)-1, 0)
-    selected_genre = genres_list[index]
+index = st.slider("Escolha o gráfico pelo índice ⬅️➡️", 0, len(genres_list)-1, 0)
 
-    st.subheader(f"🎯 Gênero selecionado: {selected_genre}")
-    st.plotly_chart(charts_json[selected_genre], use_container_width=True)
-else:
-    st.warning("⚠️ Nenhum gênero disponível em genres_list.")
+selected_genre = genres_list[index]
+st.subheader(f"🎯 Gênero selecionado: {selected_genre}")
+st.plotly_chart(charts_json[selected_genre], use_container_width=True)
 
 
 
